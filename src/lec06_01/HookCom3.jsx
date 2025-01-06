83b20ecc426db28308b28ca702c8d51e62fc4754
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-01-06 09:15:20 +0530

    commit

---
+++ src/lec06_01/HookCom3.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function SetExample3() {
+  const [color, setColor] = useState("Purple");
+  return (
+    <div>
+      <input
+        type="radio"
+        name="color"
+        onChange={() => setColor("pink")}
+      ></input>
+
+      <input
+        type="radio"
+        name="color"
+        onChange={() => setColor("orange")}
+      ></input>
+
+      <input
+        type="radio"
+        name="color"
+        onChange={() => setColor("Green")}
+      ></input>
+      <p style={{ color: color }}>Example</p>
+    </div>
+  );
+}
+
+export default SetExample3;
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,12 @@ import { createRoot } from "react-dom/client";
 // import AppFirst from "./1.jsx";
 // import FunCom1 from "./Hookcomponent1.jsx";
 // import StateExample from "./StateExample.jsx";
-import SetExample3 from "./hookcom2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <AppFirst /> */}
     {/* <FunCom1 /> */}
     {/* <StateExample /> */}
-    <SetExample3 />
+    {/* <SetExample3 /> */}
   </StrictMode>
 );
